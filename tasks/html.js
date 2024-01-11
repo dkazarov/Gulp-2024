5699c907fd4a33e510dcf3ecd28005edf605ab79
Dmitry Kazarov <d.kazarov1361@gmail.com>
2024-01-11 19:33:22 +0200

    Finish add plumber

--- tasks/html.js
+++ tasks/html.js
@@ -4,15 +4,18 @@ const browserSync = require('browser-sync');
 const size = require('gulp-size');
 const htmlmin = require('gulp-htmlmin');
 const webpHTML = require('gulp-webp-html');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
 
 const html = () => {
-  return src('./src/index.html')
-    .pipe(size({ title: 'html before compressing' }))
-    .pipe(webpHTML())
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(size({ title: 'html after compressing' }))
-    .pipe(dest('./dist'))
-    .pipe(browserSync.stream());
+	return src('./src/index.html')
+		.pipe(size({ title: 'html before compressing' }))
+		.pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+		.pipe(webpHTML())
+		.pipe(htmlmin({ collapseWhitespace: true }))
+		.pipe(size({ title: 'html after compressing' }))
+		.pipe(dest('./dist'))
+		.pipe(browserSync.stream());
 };
 
 module.exports = html;
