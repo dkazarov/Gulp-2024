49d5701dcb20089e05907e2e9d2dcd4f3923bdae
unknown <d.kazarov1361@gmail.com>
2024-09-30 21:07:26 +0300

    add many fix

--- tasks/js.js
+++ tasks/js.js
@@ -1,21 +1,21 @@
-const { src, dest } = require("gulp");
+const { src, dest } = require('gulp');
 
-const babel = require("gulp-babel");
-const size = require("gulp-size");
-const concat = require("gulp-concat");
+const babel = require('gulp-babel');
+const size = require('gulp-size');
 const webpack = require('webpack-stream');
 
 const js = () => {
-  return src("./src/**/*.js", { sourcemaps: true })
-  .pipe(babel())
-  .pipe(size({ title: "JS before compressing" }))
-  .pipe(webpack({
-    mode: 'production'
-  }))
-  .pipe(concat("main.min.js"))
-    .pipe(size({ title: "JS after compressing" }))
-    .pipe(dest("./dist/js/"));
-  //  .pipe(browserSync.stream());
+	return src('./src/**/*.js', { sourcemaps: true })
+		.pipe(babel())
+		.pipe(size({ title: 'JS before compressing' }))
+		.pipe(
+			webpack({
+				mode: 'production',
+			}),
+		)
+		.pipe(size({ title: 'JS after compressing' }))
+		.pipe(dest('./dist/js/'));
+	//  .pipe(browserSync.stream());
 };
 
 module.exports = js;
